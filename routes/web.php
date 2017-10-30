fe273c840924eb98d33df04eb03e2a81f76e72bd
HimmaYiJijiang <27532431@qq.com>
2017-10-30 12:16:27 +0800

    Finish static pages

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
 Route::post('/deploy','DeploymentController@deploy');
 
